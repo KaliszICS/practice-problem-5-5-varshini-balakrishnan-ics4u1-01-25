423037ef2a4925bcbc2233fde851ef20e2c1b674
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-21 03:06:20 +0000

    update

---
+++ src/main/java/Card.java
@@ -0,0 +1,57 @@
+class Card implements Comparable<Card> {
+    private String name;
+    private String suit;
+
+    public Card(String name, String suit) {
+        this.name = name;
+        this.suit = suit;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getSuit() {
+        return this.suit;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    @Override
+    public String toString() {
+        return this.name + " of " + this.suit;
+    }
+    @Override
+    public int compareTo(Card card) {
+        int thisSuit = suitValue(this.suit);
+        int otherSuit = suitValue(card.suit);
+        if (thisSuit != otherSuit) {
+            teturn thisSuit - otherSuit;
+        }
+        int thisRank = rankValue(this.name);
+        rankValue(card.name);
+        return thisRank - otherRank;
+    }
+    public int suitValue(String suit) {
+        if (suit.equals("Hearts")) return 0;
+        if (suit.equals("Clubs")) return 1;
+        if (suit.equals("Spades")) return 2;
+        return 3;
+    }
+    public int rankValue(String name) {
+        if(name.equals("Ace")) return 0;
+        if(name.equals("2")) return 1;
+        if(name.equals("3")) return 2;
+        if(name.equals("4")) return 3;
+        if(name.equals("5")) return 4;
+        if(name.equals("6")) return 5;
+        if(name.equals("7")) return 6;
+        if(name.equals("8")) return 7;
+        if(name.equals("9")) return 8;
+        if(name.equals("10")) return 9;
+        if(name.equals("Jack")) return 10;
+        if(name.equals("Queen")) return 11;
+        return 12;
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,37 @@
+class Student implements Comparable<Student> {
+    private String name;
+    private int age;
+    private int studentNumber;
+
+    public Student(String name, int age, int studentNumber) {
+        this.name = name;
+        this.age = age;
+        this.studentNumber = studentNumber;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public int getStudentNumber() {
+        return this.studentNumber;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setStudentNumber (int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    @Override
+    public String toString() {
+        return "N: " + this.name + "A: " + this.age + "SN: " + this.studentNumber;
+    }
+    @Override
+    public int compareTo(Student student) {
+        return this.studentNumber - student.studentNumber;
+    }
+}
